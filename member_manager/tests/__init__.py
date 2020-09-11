264fc8fa8d2fd48d3e6e1824cb46431ace404af8
jwokaty <jwokaty@users.noreply.github.com>
2020-09-11 08:36:17 -0400

    Add PublishedListFilter tests

--- member_manager/tests/__init__.py
+++ member_manager/tests/__init__.py
@@ -5,7 +5,9 @@ from member_manager.tests.availability_form import AvailabilityFormTest
 from member_manager.tests.availability_inline_formset import (
     AvailabilityInlineFormSetTest
 )
+from member_manager.tests.page import PageTest
 from member_manager.tests.profile import ProfileTest
 from member_manager.tests.profile_skill import ProfileSkillTest
+from member_manager.tests.published import PublishedListFilterTest
 from member_manager.tests.skill import SkillTest
 from member_manager.tests.utils import MemberManagerUtilsTest
