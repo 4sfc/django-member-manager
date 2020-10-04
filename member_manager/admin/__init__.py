7ce828730ea368c2f6d30eda8567c75f6119e118
jwokaty <jwokaty@users.noreply.github.com>
2020-10-04 01:31:37 -0400

    Add HowHeardAdmin closes #23

--- member_manager/admin/__init__.py
+++ member_manager/admin/__init__.py
@@ -2,6 +2,7 @@
 
 from member_manager.admin.availability import AvailabilityAdmin
 from member_manager.admin.availability_inline import AvailabilityInline
+from member_manager.admin.how_heard import HowHeardAdmin
 from member_manager.admin.page import PageAdmin
 from member_manager.admin.profile import ProfileAdmin
 from member_manager.admin.profile_skill_inline import ProfileSkillInline
